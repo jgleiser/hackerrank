edc0c45886dca76974a307c10d4dde90ec2423b5
Jose Gleiser <jose.gleiser@gmail.com>
2017-06-03 17:29:29 -0400

    30 days of code 29 final

---
+++ 30 Days of Code Challenges/29 Bitwise AND.js
@@ -0,0 +1,38 @@
+// https://www.hackerrank.com/challenges/30-bitwise-and
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var n_temp = readLine().split(' ');
+        var n = parseInt(n_temp[0]);
+        var k = parseInt(n_temp[1]);
+        
+        if ((k-1 | k) > n) {
+            console.log(k-2);
+        } else {
+            console.log(k-1);
+        }
+    }
+}
